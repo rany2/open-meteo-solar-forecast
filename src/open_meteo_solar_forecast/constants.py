9624c73170e0df39478b60a02e948a63e90d64d4
rany <ranygh@riseup.net>
2024-06-02 19:08:38 +0300

    Remove cloud cover compensation code
    It doesn't seem to be working well:
    https://community.home-assistant.io/t/open-meteo-solar-forecast/733073/13
    
    Signed-off-by: rany <ranygh@riseup.net>

--- src/open_meteo_solar_forecast/constants.py
+++ src/open_meteo_solar_forecast/constants.py
@@ -1,8 +1,10 @@
 """Constants for the solar forecast module."""
 
 # STC specifies a cell temperature of 25°C and an irradiance of 1000 W/m².
-# NOCT specifies a cell temperature of 45°C and an irradiance of 800 W/m².
-# The temperature coefficient of the solar panel is 0.004°C⁻¹.
+# NOCT specifies a cell temperature of 45°C, an irradiance of 800 W/m²
+# and an ambient temperature of 20°C.
+#
+# The temperature coefficient of most solar panels is 0.004°C⁻¹.
 #
 # Source: https://www.researchgate.net/publication/372240079_Solar_Prediction_Strategy_for_Managing_Virtual_Power_Stations
 ALPHA_TEMP = -0.004  # °C-1
