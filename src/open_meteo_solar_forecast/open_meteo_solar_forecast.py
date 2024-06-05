rany2/open-meteo-solar-forecast

commit 9ec441d87bf54014b114a71a86c97a4ffc3697d1

Account for cell efficiency and wind speed for cell temperature calculations
Signed-off-by: rany <ranygh@riseup.net>

pyproject.toml
@@ -1,6 +1,6 @@
 [tool.poetry]
 name = "open-meteo-solar-forecast"
-version = "0.1.8"
+version = "0.1.9"
 description = "Asynchronous Python client for getting forecast solar information"
 authors = ["Rany <rany@riseup.net>", "Klaas Schoute <hello@student-techlife.com>"]
 maintainers = ["Rany <rany@riseup.net>"]

src/open_meteo_solar_forecast/open_meteo_solar_forecast.py
@@ -11,11 +11,14 @@ from aiohttp import ClientSession
 
 from .constants import (
     ALPHA_TEMP,
+    CELL_EFFICIENCY,
     G_NOCT,
     G_STC,
     TEMP_NOCT_AMB,
     TEMP_NOCT_CELL,
     TEMP_STC_CELL,
+    TRANSMITTANCE_ABSORPTION,
+    WIND_NOCT_SPEED,
 )
 from .exceptions import (
     OpenMeteoSolarForecastAuthenticationError,
@@ -140,7 +143,7 @@ class OpenMeteoSolarForecast:
             "longitude": str(self.longitude),
             "azimuth": str(self.azimuth),
             "tilt": str(self.declination),
-            "minutely_15": "temperature_2m"
+            "minutely_15": "temperature_2m,wind_speed_10m"
             ",global_tilted_irradiance,global_tilted_irradiance_instant",
             "forecast_days": str(self.forecast_days),
             "past_days": str(self.past_days),
@@ -153,6 +156,10 @@ class OpenMeteoSolarForecast:
         gti_avg_arr = data["minutely_15"]["global_tilted_irradiance"]
         gti_instant_arr = data["minutely_15"]["global_tilted_irradiance_instant"]
         temp_arr = data["minutely_15"]["temperature_2m"]
+        wind_arr = [
+            wind_speed * 1000 / 3600
+            for wind_speed in data["minutely_15"]["wind_speed_10m"]
+        ]
         utc_offset = data["utc_offset_seconds"]
         time_arr = [
             dt.strptime(time, "%Y-%m-%dT%H:%M").replace(
@@ -163,20 +170,34 @@ class OpenMeteoSolarForecast:
 
         peak_power = self.kwp * 1000  # Convert kW to W
 
-        power_avg: dict[dt, int] = {}
-        watts_instant: dict[dt, int] = {}
+        w_avg: dict[dt, int] = {}
+        w_inst: dict[dt, int] = {}
         wh_days: dict[dt, int] = {}
 
-        def gen_power(gti: float, t_amb: float) -> int:
+        def gen_power(gti: float, t_amb: float, wind_speed: float) -> int:
             """Calculate the power generated by a solar panel.
 
             Formulas:
             ---------
-                Tc=Ta + (NOCT-Tnoct) / Gnoct*G  (p.509)
+                Tc=Ta + (G/Gnoct) * (UL,noct/UL) * (Tnoct - Ta,noct) * (1 - ηcell/τα)
+                τα=0.9
+                ηcell=efficiency of the cell (assumed polycrystalline, so 12%)
+                UL,noct/UL = hw,noct/hw,v
+                hw(v) = 8.91 + 2.0 * v
+                Source: https://crimsonpublishers.com/prsp/pdf/PRSP.000528.pdf
+
                 P=Pmax * (G/Gstc) * (1 + α * (Tc-Tstc)) * ηDC (p.509)
                 Source: https://www.researchgate.net/publication/372240079_Solar_Prediction_Strategy_for_Managing_Virtual_Power_Stations
             """
-            temp_cell = t_amb + (TEMP_NOCT_CELL - TEMP_NOCT_AMB) / G_NOCT * gti
+
+            def hw_v(v: float) -> float:
+                return 8.91 + 2.0 * v
+
+            temp_cell = gti / G_NOCT
+            temp_cell *= hw_v(WIND_NOCT_SPEED) / hw_v(wind_speed)
+            temp_cell *= TEMP_NOCT_CELL - t_amb
+            temp_cell *= 1 - (CELL_EFFICIENCY / TRANSMITTANCE_ABSORPTION)
+            temp_cell += t_amb
             power = (
                 peak_power
                 * (gti / G_STC)
@@ -190,28 +211,33 @@ class OpenMeteoSolarForecast:
                 gti_avg_arr[i],
                 gti_instant_arr[i],
                 *(temp_arr[i], temp_arr[i - 1] if i > 0 else temp_arr[i]),
+                *(wind_arr[i], wind_arr[i - 1] if i > 0 else wind_arr[i]),
             ):
                 continue
 
             # Total radiation received on a tilted pane
-            gti_avg = gti_avg_arr[i]
-            gti_instant = gti_instant_arr[i]
+            g_avg = gti_avg_arr[i]
+            g_inst = gti_instant_arr[i]
 
-            # Get the temperature
-            temp_instant = temp_arr[i - 1] if i > 0 else temp_arr[i]
+            # Get the temperature and wind speed for instant and average values
             temp_avg = (temp_arr[i] + temp_arr[i - 1]) / 2 if i > 0 else temp_arr[i]
+            wind_avg = (wind_arr[i] + wind_arr[i - 1]) / 2 if i > 0 else wind_arr[i]
+            temp_inst = temp_arr[i]
+            wind_inst = wind_arr[i]
 
-            # For minutely data, the time_start is 15 minutes before the current time
-            time_start = time - timedelta(minutes=15)
+            # For minutely data, the average is taken over 15 minutes whereas
+            # the instant data is for the current minute only
+            time_start_inst = time
+            time_start_avg = time - timedelta(minutes=15)
 
             # Calculate and store the power generated
-            power_avg[time_start] = gen_power(gti_avg, temp_avg)
-            watts_instant[time_start] = gen_power(gti_instant, temp_instant)
+            w_avg[time_start_avg] = gen_power(g_avg, temp_avg, wind_avg)
+            w_inst[time_start_inst] = gen_power(g_inst, temp_inst, wind_inst)
 
         # Calculate the average power generated per hour
         wh_period: dict[dt, int] = {}
         wh_period_count: dict[dt, int] = {}
-        for time, power in power_avg.items():
+        for time, power in w_avg.items():
             hour = time.replace(minute=0, second=0, microsecond=0)
             wh_period[hour] = wh_period.get(hour, 0) + power
             wh_period_count[hour] = wh_period_count.get(hour, 0) + 1
@@ -225,7 +251,7 @@ class OpenMeteoSolarForecast:
 
         # Return the estimate object
         return Estimate(
-            watts=watts_instant,
+            watts=w_inst,
             wh_period=wh_period,
             wh_days=wh_days,
             api_timezone=timezone(timedelta(seconds=utc_offset)),

src/open_meteo_solar_forecast/constants.py
@@ -1,6 +1,7 @@
 """Constants for the solar forecast module."""
 
 # STC specifies a cell temperature of 25°C and an irradiance of 1000 W/m².
+#
 # NOCT specifies a cell temperature of 45°C, an irradiance of 800 W/m²
 # and an ambient temperature of 20°C.
 #
@@ -13,3 +14,18 @@ G_STC = 1000.0  # W/m2
 TEMP_NOCT_AMB = 20.0  # °C
 TEMP_NOCT_CELL = 45.0  # °C
 TEMP_STC_CELL = 25.0  # °C
+
+# This is used to calculate the temperature of the solar cell.
+#
+# Monocrystalline PV silicon cells: 15%
+# Multi-crystalline (or polycrystalline) silicon: 12%
+# Amorphous series of silicon: 6%
+#
+# Wind speed is assumed to be 1 m/s in the NOCT conditions.
+#
+# Source:
+# - https://www.researchgate.net/publication/343126399_Effect_of_temperature_and_wind_on_PV_Module's_efficiency_Energy_and_Resource_Utilization
+# - https://crimsonpublishers.com/prsp/pdf/PRSP.000528.pdf
+CELL_EFFICIENCY = 0.12  # Multi-crystalline assumed
+WIND_NOCT_SPEED = 1.0  # m/s
+TRANSMITTANCE_ABSORPTION = 0.9  # τα

commit 68e28b6adf4a5dd9f76f32df73125c88ed949e42

Fix typo introduced by 9ec441d87bf5
Signed-off-by: rany <ranygh@riseup.net>

pyproject.toml
@@ -1,6 +1,6 @@
 [tool.poetry]
 name = "open-meteo-solar-forecast"
-version = "0.1.9"
+version = "0.1.10"
 description = "Asynchronous Python client for getting forecast solar information"
 authors = ["Rany <rany@riseup.net>", "Klaas Schoute <hello@student-techlife.com>"]
 maintainers = ["Rany <rany@riseup.net>"]

src/open_meteo_solar_forecast/open_meteo_solar_forecast.py
@@ -195,7 +195,7 @@ class OpenMeteoSolarForecast:
 
             temp_cell = gti / G_NOCT
             temp_cell *= hw_v(WIND_NOCT_SPEED) / hw_v(wind_speed)
-            temp_cell *= TEMP_NOCT_CELL - t_amb
+            temp_cell *= TEMP_NOCT_CELL - TEMP_NOCT_AMB
             temp_cell *= 1 - (CELL_EFFICIENCY / TRANSMITTANCE_ABSORPTION)
             temp_cell += t_amb
             power = (