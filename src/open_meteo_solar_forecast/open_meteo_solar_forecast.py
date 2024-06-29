rany2/open-meteo-solar-forecast

commit 2ae2afa7b082ee26b550cdcc6ff02e3c653378c4

Implement support for multiple PV arrays (#8)
Fixes #5

Signed-off-by: rany <ranygh@riseup.net>

src/open_meteo_solar_forecast/open_meteo_solar_forecast.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+from collections import defaultdict
 from dataclasses import dataclass
 from datetime import datetime as dt
 from datetime import timedelta, timezone
@@ -35,11 +36,11 @@ from .models import Estimate
 class OpenMeteoSolarForecast:
     """Main class for handling connections with the API."""
 
-    azimuth: float
-    declination: float
-    dc_kwp: float
-    latitude: float
-    longitude: float
+    azimuth: float | list[float]
+    declination: float | list[float]
+    dc_kwp: float | list[float]
+    latitude: float | list[float]
+    longitude: float | list[float]
 
     past_days: int = 92
     forecast_days: int = 16
@@ -47,7 +48,7 @@ class OpenMeteoSolarForecast:
     ac_kwp: float | None = None
     api_key: str | None = None
     base_url: str | None = None
-    efficiency_factor: float = 1.0
+    efficiency_factor: float | list[float] = 1.0
 
     session: ClientSession | None = None
     _close_session: bool = False
@@ -57,7 +58,47 @@ class OpenMeteoSolarForecast:
         if self.base_url is None:
             self.base_url = "https://api.open-meteo.com"
         if self.ac_kwp is None:
-            self.ac_kwp = self.dc_kwp
+            self.ac_kwp = float("inf")
+
+        # Validate list parameters
+        if list in map(
+            type,
+            (
+                self.azimuth,
+                self.declination,
+                self.dc_kwp,
+                self.latitude,
+                self.longitude,
+            ),
+        ):
+            if not all(
+                isinstance(param, list) and len(param) == len(self.dc_kwp)
+                for param in (
+                    self.azimuth,
+                    self.declination,
+                    self.dc_kwp,
+                    self.latitude,
+                    self.longitude,
+                )
+            ):
+                raise OpenMeteoSolarForecastConfigError(
+                    "The parameters must be of the same length"
+                )
+        else:
+            self.azimuth = [self.azimuth]
+            self.declination = [self.declination]
+            self.dc_kwp = [self.dc_kwp]
+            self.latitude = [self.latitude]
+            self.longitude = [self.longitude]
+
+        if isinstance(self.efficiency_factor, list):
+            if len(self.efficiency_factor) != len(self.dc_kwp):
+                raise OpenMeteoSolarForecastConfigError(
+                    "The efficiency factor must be of the same length as the other "
+                    "parameters"
+                )
+        else:
+            self.efficiency_factor = [self.efficiency_factor] * len(self.dc_kwp)
 
     async def _request(
         self,
@@ -141,45 +182,11 @@ class OpenMeteoSolarForecast:
             A Estimate object, with a estimated production forecast.
 
         """
-        params = {
-            "latitude": str(self.latitude),
-            "longitude": str(self.longitude),
-            "azimuth": str(self.azimuth),
-            "tilt": str(self.declination),
-            "minutely_15": "temperature_2m,wind_speed_10m"
-            ",global_tilted_irradiance,global_tilted_irradiance_instant",
-            "forecast_days": str(self.forecast_days),
-            "past_days": str(self.past_days),
-            "timezone": "auto",
-        }
-        data = await self._request(
-            "/v1/forecast",
-            params=params,
-        )
-        gti_avg_arr = data["minutely_15"]["global_tilted_irradiance"]
-        gti_inst_arr = data["minutely_15"]["global_tilted_irradiance_instant"]
-        temp_arr = data["minutely_15"]["temperature_2m"]
-        wind_arr = [
-            wind_speed * 1000 / 3600 if wind_speed is not None else None
-            for wind_speed in data["minutely_15"]["wind_speed_10m"]
-        ]
-        utc_offset = data["utc_offset_seconds"]
-        time_arr = [
-            dt.strptime(time, "%Y-%m-%dT%H:%M").replace(
-                tzinfo=timezone(timedelta(seconds=utc_offset))
-            )
-            for time in data["minutely_15"]["time"]
-        ]
+        w_avg: dict[dt, int] = defaultdict(int)
+        w_inst: dict[dt, int] = defaultdict(int)
+        wh_days: dict[dt, int] = defaultdict(int)
 
-        # Convert kW to W
-        dc_wp = self.dc_kwp * 1000
-        ac_wp = self.ac_kwp * 1000
-
-        w_avg: dict[dt, int] = {}
-        w_inst: dict[dt, int] = {}
-        wh_days: dict[dt, int] = {}
-
-        def gen_power(gti: float, t_amb: float, wind_speed: float) -> int:
+        def gen_power(gti: float, t_amb: float, wind_speed: float, eff: float) -> int:
             """Calculate the power generated by a solar panel.
 
             Formulas:
@@ -206,41 +213,96 @@ class OpenMeteoSolarForecast:
             power = dc_wp
             power *= gti / G_STC
             power *= 1 + ALPHA_TEMP * (temp_cell - TEMP_STC_CELL)
-            power *= self.efficiency_factor
-            return round(max(0, min(power, ac_wp)))
+            power *= eff
+            return round(max(0, power))
 
-        for i, time in enumerate(time_arr):
-            # Skip the first element as we need the previous element to calculate
-            # the average temperature and wind speed for the current time
-            if i - 1 < 0:
-                continue
+        utc_offset = None
+        for az, dec, dc_kwp, lat, lon, eff in zip(
+            self.azimuth,
+            self.declination,
+            self.dc_kwp,
+            self.latitude,
+            self.longitude,
+            self.efficiency_factor,
+            strict=True,
+        ):
+            params = {
+                "latitude": str(lat),
+                "longitude": str(lon),
+                "azimuth": str(az),
+                "tilt": str(dec),
+                "minutely_15": "temperature_2m,wind_speed_10m"
+                ",global_tilted_irradiance,global_tilted_irradiance_instant",
+                "forecast_days": str(self.forecast_days),
+                "past_days": str(self.past_days),
+                "timezone": "auto",
+            }
+            data = await self._request(
+                "/v1/forecast",
+                params=params,
+            )
+            gti_avg_arr = data["minutely_15"]["global_tilted_irradiance"]
+            gti_inst_arr = data["minutely_15"]["global_tilted_irradiance_instant"]
+            temp_arr = data["minutely_15"]["temperature_2m"]
+            wind_arr = [
+                wind_speed * 1000 / 3600
+                for wind_speed in data["minutely_15"]["wind_speed_10m"]
+            ]
+            if utc_offset is None:
+                utc_offset = data["utc_offset_seconds"]
+            elif utc_offset != data["utc_offset_seconds"]:
+                raise OpenMeteoSolarForecastConfigError(
+                    "The UTC offset is not the same for all locations"
+                )
+            time_arr = [
+                dt.strptime(time, "%Y-%m-%dT%H:%M").replace(
+                    tzinfo=timezone(timedelta(seconds=utc_offset))
+                )
+                for time in data["minutely_15"]["time"]
+            ]
 
-            # Skip none-values
-            if None in (
-                gti_avg_arr[i],
-                gti_inst_arr[i],
-                *temp_arr[i - 1 : i + 1],
-                *wind_arr[i - 1 : i + 1],
-            ):
-                continue
+            # Convert kW to W
+            dc_wp = dc_kwp * 1000
 
-            # Get the global tilted irradiance for average and instantaneous values
-            g_avg = gti_avg_arr[i]
-            g_inst = gti_inst_arr[i]
+            for i, time in enumerate(time_arr):
+                # Skip the first element as we need the previous element to calculate
+                # the average temperature and wind speed for the current time
+                if i - 1 < 0:
+                    continue
 
-            # Get the temperature and wind speed for average and instantaneous values
-            temp_avg = (temp_arr[i] + temp_arr[i - 1]) / 2
-            wind_avg = (wind_arr[i] + wind_arr[i - 1]) / 2
-            temp_inst = temp_arr[i - 1]
-            wind_inst = wind_arr[i - 1]
+                # Skip none-values
+                if None in (
+                    gti_avg_arr[i],
+                    gti_inst_arr[i],
+                    *temp_arr[i - 1 : i + 1],
+                    *wind_arr[i - 1 : i + 1],
+                ):
+                    continue
 
-            # For minutely data, the radiation start time is 15 minutes before the time
-            # even for instantaneous data (since the data is averaged over 15 minutes)
-            time_start = time - timedelta(minutes=15)
+                # Get the GTI for average and instantaneous values
+                g_avg = gti_avg_arr[i]
+                g_inst = gti_inst_arr[i]
 
-            # Calculate and store the power generated
-            w_avg[time_start] = gen_power(g_avg, temp_avg, wind_avg)
-            w_inst[time_start] = gen_power(g_inst, temp_inst, wind_inst)
+                # Get the temp and wind speed for average and instantaneous values
+                temp_avg = (temp_arr[i] + temp_arr[i - 1]) / 2
+                wind_avg = (wind_arr[i] + wind_arr[i - 1]) / 2
+                temp_inst = temp_arr[i - 1]
+                wind_inst = wind_arr[i - 1]
+
+                # For minutely data, the GTI start time is 15 minutes before the time
+                # even for instant data (since the data is averaged over 15 minutes)
+                time_start = time - timedelta(minutes=15)
+
+                # Calculate and store the power generated
+                w_avg[time_start] += gen_power(g_avg, temp_avg, wind_avg, eff)
+                w_inst[time_start] += gen_power(g_inst, temp_inst, wind_inst, eff)
+
+        # Clamp the power generated to the AC power
+        ac_wp = self.ac_kwp * 1000  # Convert kW to W
+        for time in w_avg:
+            w_avg[time] = min(w_avg[time], ac_wp)
+        for time in w_inst:
+            w_inst[time] = min(w_inst[time], ac_wp)
 
         # Calculate the average power generated per hour
         wh_period: dict[dt, int] = {}

commit 73b33b40a0f6e9edfd1d9f253adcede47a6a7410

Skip reintroduced wind speed None exception bug
File "/usr/src/homeassistant/homeassistant/helpers/update_coordinator.py", line 315, in _async_refresh
self.data = await self._async_update_data()
            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
File "/config/custom_components/open_meteo_solar_forecast/coordinator.py", line 57, in _async_update_data
return await self.forecast.estimate()
       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
File "/.local/lib/python3.12/site-packages/open_meteo_solar_forecast/open_meteo_solar_forecast.py", line 160, in estimate
wind_speed * 1000 / 3600
~~~~~~~~~~~^~~~~~
TypeError: unsupported operand type(s) for *: 'NoneType' and 'int'

Fixes: 2ae2afa ("Implement support for multiple PV arrays (#8)")
Signed-off-by: rany <ranygh@riseup.net>

src/open_meteo_solar_forecast/open_meteo_solar_forecast.py
@@ -264,7 +264,7 @@ class OpenMeteoSolarForecast:
             gti_inst_arr = data["minutely_15"]["global_tilted_irradiance_instant"]
             temp_arr = data["minutely_15"]["temperature_2m"]
             wind_arr = [
-                wind_speed * 1000 / 3600
+                wind_speed * 1000 / 3600 if wind_speed is not None else None
                 for wind_speed in data["minutely_15"]["wind_speed_10m"]
             ]
 