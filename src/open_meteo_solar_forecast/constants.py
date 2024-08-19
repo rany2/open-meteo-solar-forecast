e61d5893656b7c3cded252e9543101a0393f4773
Rany <ranygh@riseup.net>
2024-08-19 17:10:46 +0300

    Use Ross Model for cell temperature calculation (#16)
    This yields more accurate results in my case.
    
    Signed-off-by: rany <ranygh@riseup.net>

--- src/open_meteo_solar_forecast/constants.py
+++ src/open_meteo_solar_forecast/constants.py
@@ -1,5 +1,7 @@
 """Constants for the solar forecast module."""
 
+from dataclasses import dataclass
+
 # STC specifies a cell temperature of 25°C and an irradiance of 1000 W/m².
 #
 # NOCT specifies a cell temperature of 45°C, an irradiance of 800 W/m²
@@ -8,6 +10,8 @@
 # The temperature coefficient of most solar panels is 0.004°C⁻¹.
 #
 # Source: https://www.researchgate.net/publication/372240079_Solar_Prediction_Strategy_for_Managing_Virtual_Power_Stations
+
+
 ALPHA_TEMP = -0.004  # °C-1
 G_NOCT = 800.0  # W/m2
 G_STC = 1000.0  # W/m2
@@ -15,17 +19,18 @@ TEMP_NOCT_AMB = 20.0  # °C
 TEMP_NOCT_CELL = 45.0  # °C
 TEMP_STC_CELL = 25.0  # °C
 
-# This is used to calculate the temperature of the solar cell.
+
+# Ross Model Constants
 #
-# Monocrystalline PV silicon cells: 15%
-# Multi-crystalline (or polycrystalline) silicon: 12%
-# Amorphous series of silicon: 6%
-#
-# Wind speed is assumed to be 1 m/s in the NOCT conditions.
-#
-# Source:
-# - https://www.researchgate.net/publication/343126399_Effect_of_temperature_and_wind_on_PV_Module's_efficiency_Energy_and_Resource_Utilization
-# - https://crimsonpublishers.com/prsp/pdf/PRSP.000528.pdf
-CELL_EFFICIENCY = 0.12  # Multi-crystalline assumed
-WIND_NOCT_SPEED = 1.0  # m/s
-TRANSMITTANCE_ABSORPTION = 0.9  # τα
+# Source: https://www.researchgate.net/publication/275438802_Thermal_effects_of_the_extended_holographic_regions_for_holographic_planar_concentrator
+@dataclass
+class RossModelConstants:
+    """Constants for the Ross model."""
+
+    WELL_COOLED = 0.0200
+    FREE_STANDING = 0.0208
+    FLAT_ON_ROOF = 0.0260
+    NOT_SO_WELL_COOLED = 0.0342
+    TRANSPARENT_PV = 0.0455
+    FACADE_INTEGRATED = 0.0538
+    ON_SLOPED_ROOF = 0.0563
