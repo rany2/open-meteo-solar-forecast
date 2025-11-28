d890d591d347993f4fc88cab6701a7452670dc86
Thomas May <thomay@posteo.de>
2025-11-28 19:08:02 +0100

    Add horizon profile from text file (#37)
    * horizon map implemented, still hard-coded
    
    * horizon feature made configurable, added note to Readme
    
    * cleanup files
    
    * Change default values for use_horizon and horizon_map
    
    * Temporary change for debugging
    
    Set irradiance values to zero when horizon shading is present.
    
    * Update project metadata and repository links
    
    for debugging of HA connection only
    
    * Revert "Update project metadata and repository links"
    
    This reverts commit e40b6dc01ddfa2b98c702d950b4ff6e75da1a5db.
    
    * Revert "Temporary change for debugging"
    
    This reverts commit cfe45a5b12c6b312ce9732df366f4a5945dc2b77.
    
    * Revert "Temporary change for debugging"
    
    This reverts commit cfe45a5b12c6b312ce9732df366f4a5945dc2b77.
    
    * horizon profile is now specified in horizon.txt
    
    * Update README with horizon map text file info
    
    Added information about passing the horizon map from a text file in the README.
    
    * update poetry/pyproject
    
    Signed-off-by: rany <ranygh@riseup.net>
    
    ---------
    
    Signed-off-by: rany <ranygh@riseup.net>
    Co-authored-by: rany <ranygh@riseup.net>

--- src/open_meteo_solar_forecast/open_meteo_solar_forecast.py
+++ src/open_meteo_solar_forecast/open_meteo_solar_forecast.py
@@ -47,7 +47,7 @@ class OpenMeteoSolarForecast:
     damping_evening: float | list[float] = 0.0
     efficiency_factor: float | list[float] = 1.0
     use_horizon: bool | list[bool] = False
-    horizon_map: tuple(tuple(float)) | list[tuple(tuple(float))] = ((0,0),(360,0))
+    horizon_map: tuple(tuple(float)) | list[tuple(tuple(float))] = ((0.0,20.0),(360.0,20.0))
 
     session: ClientSession | None = None
     _close_session: bool = False
